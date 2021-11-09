f5e8dbd566b0fe9a41ed6c953763726e28f86418
the_nza <the_nza@protonmail.com>
2021-11-09 10:10:44 +0300

    Descrease aside's div border radius. Add some conditional statement for product archive sidebar.

--- igenomix_store/sidebar-product-archive.php
+++ igenomix_store/sidebar-product-archive.php
@@ -20,13 +20,25 @@ if ( is_product_category() ) {
 <!-- Site aside -->
 <aside class="prodarchive__aside">
 	<nav class="prodarchive__asidenav">
+	<?php if(count($childCategories) > 0) : ?>
+
 		<ul class="asidenav">
 			<?php foreach($childCategories as $chCat) : ?>
 			<li class="asidenav__item">
-				<a class="<?php echo $chCat->term_id === $currentCategory->term_id ? 'active' : '';?>" href="<?php echo get_category_link($chCat->term_id) ?>"><?php echo $chCat->name;?></a>
+				<a class="<?php echo $chCat->term_id === $currentCategory->term_id ? 'active' : '';?>"
+				   href="<?php echo get_category_link($chCat->term_id) ?>"
+				>
+					<?php echo $chCat->name;?>
+				</a>
 			</li>
 			<?php endforeach; ?>
 		</ul>
+
+	<?php else : ?>
+
+		<h4>No child categories</h4>
+
+	<?php endif ; ?>
 	</nav>
 	<?php /* dynamic_sidebar( 'sidebar-1' ); */ ?>
 </aside>
