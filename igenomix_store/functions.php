c90d13f9717ced46b8436b755e02c14bdfaecee9
the_nza <the_nza@protonmail.com>
2021-11-12 22:08:22 +0300

    Add styles for single product page

--- igenomix_store/functions.php
+++ igenomix_store/functions.php
@@ -168,9 +168,6 @@ add_action("init", function (){
 	remove_filter( 'woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20 );
 	add_action( 'ignx_dispay_related_products', 'woocommerce_output_related_products', 5 );
 	remove_filter( 'woocommerce_after_single_product_summary', 'woocommerce_upsell_display', 15 );
-
-
-
 });
 
 // * Set number of related products
