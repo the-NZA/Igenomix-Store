90977402639be2bbcb0226ee7c88c6d082a46078
the_nza <the_nza@protonmail.com>
2021-11-22 17:02:04 +0300

    Finish cart main section basic styles

--- igenomix_store/woocommerce/cart/cart.php
+++ igenomix_store/woocommerce/cart/cart.php
@@ -65,32 +65,33 @@ do_action( 'woocommerce_before_cart' ); ?>
 							?>
 							</div>
 
-							<div class="carttableitem__price product-price" data-title="<?php esc_attr_e( 'Price', 'woocommerce' ); ?>">
+							<div class="carttable__price_quantity">
+								<div class="carttableitem__price product-price" data-title="<?php esc_attr_e( 'Price', 'woocommerce' ); ?>">
+									<?php
+										displayCartItemPrice($_product, "₽")
+									?>
+								</div>
+								<div class="carttableitem__quantity product-quantity" data-title="<?php esc_attr_e( 'Quantity', 'woocommerce' ); ?>">
 								<?php
-									displayCartItemPrice($_product, "₽")
+								if ( $_product->is_sold_individually() ) {
+									$product_quantity = sprintf( '1 <input type="hidden" name="cart[%s][qty]" value="1" />', $cart_item_key );
+								} else {
+									$product_quantity = woocommerce_quantity_input(
+										array(
+											'input_name'   => "cart[{$cart_item_key}][qty]",
+											'input_value'  => $cart_item['quantity'],
+											'max_value'    => $_product->get_max_purchase_quantity(),
+											'min_value'    => '0',
+											'product_name' => $_product->get_name(),
+										),
+										$_product,
+										false
+									);
+								}
+
+								echo apply_filters( 'woocommerce_cart_item_quantity', $product_quantity, $cart_item_key, $cart_item ); // PHPCS: XSS ok.
 								?>
-							</div>
-
-							<div class="carttableitem__quantity product-quantity" data-title="<?php esc_attr_e( 'Quantity', 'woocommerce' ); ?>">
-							<?php
-							if ( $_product->is_sold_individually() ) {
-								$product_quantity = sprintf( '1 <input type="hidden" name="cart[%s][qty]" value="1" />', $cart_item_key );
-							} else {
-								$product_quantity = woocommerce_quantity_input(
-									array(
-										'input_name'   => "cart[{$cart_item_key}][qty]",
-										'input_value'  => $cart_item['quantity'],
-										'max_value'    => $_product->get_max_purchase_quantity(),
-										'min_value'    => '0',
-										'product_name' => $_product->get_name(),
-									),
-									$_product,
-									false
-								);
-							}
-
-							echo apply_filters( 'woocommerce_cart_item_quantity', $product_quantity, $cart_item_key, $cart_item ); // PHPCS: XSS ok.
-							?>
+								</div>
 							</div>
 
 							<div class="carttableitem__subtotal product-subtotal" data-title="<?php esc_attr_e( 'Subtotal', 'woocommerce' ); ?>">
@@ -130,7 +131,8 @@ do_action( 'woocommerce_before_cart' ); ?>
 		<div class="cartform__meta">
 			<?php if ( wc_coupons_enabled() ) { ?>
 				<div class="coupon">
-					<label for="coupon_code"><?php esc_html_e( 'Coupon:', 'woocommerce' ); ?></label> <input type="text" name="coupon_code" class="input-text" id="coupon_code" value="" placeholder="<?php esc_attr_e( 'Coupon code', 'woocommerce' ); ?>" /> <button type="submit" class="button" name="apply_coupon" value="<?php esc_attr_e( 'Apply coupon', 'woocommerce' ); ?>"><?php esc_attr_e( 'Apply coupon', 'woocommerce' ); ?></button>
+					<!-- <label for="coupon_code"><?php esc_html_e( 'Coupon:', 'woocommerce' ); ?></label> -->
+					<input type="text" name="coupon_code" class="input-text" id="coupon_code" value="" placeholder="<?php esc_attr_e( 'Coupon code', 'woocommerce' ); ?>" /> <button type="submit" class="button" name="apply_coupon" value="<?php esc_attr_e( 'Apply coupon', 'woocommerce' ); ?>"><?php esc_attr_e( 'Apply coupon', 'woocommerce' ); ?></button>
 					<?php do_action( 'woocommerce_cart_coupon' ); ?>
 				</div>
 			<?php } ?>
