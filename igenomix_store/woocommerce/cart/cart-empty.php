f38d0578b4a9edb94ac7b9fcc03e29504cd5a470
the_nza <the_nza@protonmail.com>
2021-12-23 10:33:17 +0300

    Add styles for empty cart page content

--- igenomix_store/woocommerce/cart/cart-empty.php
+++ igenomix_store/woocommerce/cart/cart-empty.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Empty cart page
  *
@@ -7,25 +8,30 @@
  * @version 3.5.0
  */
 
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
-/*
- * @hooked wc_empty_cart_message - 10
- */
-do_action( 'woocommerce_cart_is_empty' );
+?>
 
-if ( wc_get_page_id( 'shop' ) > 0 ) : ?>
-	<p class="return-to-shop">
-		<a class="button wc-backward" href="<?php echo esc_url( apply_filters( 'woocommerce_return_to_shop_redirect', wc_get_page_permalink( 'shop' ) ) ); ?>">
-			<?php
+<div class="cartpage__empty">
+	<?php
+	/*
+	 * @hooked wc_empty_cart_message - 10
+	 */
+	do_action('woocommerce_cart_is_empty');
+
+	if (wc_get_page_id('shop') > 0) : ?>
+		<div class="return-to-shop">
+			<a class="button wc-backward" href="<?php echo esc_url(apply_filters('woocommerce_return_to_shop_redirect', wc_get_page_permalink('shop'))); ?>">
+				<?php
 				/**
 				 * Filter "Return To Shop" text.
 				 *
 				 * @since 4.6.0
 				 * @param string $default_text Default text.
 				 */
-				echo esc_html( apply_filters( 'woocommerce_return_to_shop_text', __( 'Return to shop', 'woocommerce' ) ) );
-			?>
-		</a>
-	</p>
-<?php endif; ?>
+				echo esc_html(apply_filters('woocommerce_return_to_shop_text', __('Return to shop', 'woocommerce')));
+				?>
+			</a>
+		</div>
+	<?php endif; ?>
+</div>
