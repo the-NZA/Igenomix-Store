aac9410e959e98a04fd2ec85690a29c072479bd4
the_nza <the_nza@protonmail.com>
2021-12-24 13:53:36 +0300

    Fix bug in script

--- font_path_replacement.sh
+++ font_path_replacement.sh
@@ -2,7 +2,7 @@
 
 if [ -n "$1" ]
 then
-	sed -i '' 's/\/font/\/wp-content\/themes\/igenomix_store\/assets\/font/g' $1
+	sed -i '' 's/\/font\//\/wp-content\/themes\/igenomix_store\/assets\/font\//g' $1
 else
 	echo "Path not found"
 fi
