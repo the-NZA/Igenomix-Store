36bc1048b71575f6e63b28db0e797e85f0689f6c
the_nza <the_nza@protonmail.com>
2022-01-15 11:31:32 +0300

    Fix handeling for child categories

--- igenomix_store/woocommerce/loop/loop-start.php
+++ igenomix_store/woocommerce/loop/loop-start.php
@@ -10,15 +10,25 @@ if (!defined('ABSPATH')) {
 }
 
 $isShop = is_shop();
-$hasChildCats = haveChildCategories();
 ?>
 <!--Site main-->
 <main class="products site-main prodarchive__main">
 	<!-- Archive card list -->
 	<?php if ($isShop) : ?>
+
 		<div class="prodarchive__list">
-	<?php elseif (!$hasChildCats) : ?>
-		<div class="prodarchive__list">
+
 	<?php else : ?>
-		<div class="prodarchive__list prodarchive__list--single">
+		<?php
+		$hasChildCats = haveChildCategories();
+
+		if (!$hasChildCats) : ?>
+
+			<div class="prodarchive__list">
+
+		<?php else : ?>
+
+			<div class="prodarchive__list prodarchive__list--single">
+
+		<?php endif; ?>
 	<?php endif; ?>
